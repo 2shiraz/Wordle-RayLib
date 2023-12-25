6595cfeb61cd3cabd547e8aa2aaa1fe66e89bd9b
Shiraz Khan <shirazahmad200@gmail.com>
2023-12-25 23:11:59 +0500

    KeyBoard Layout fixed

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+const int screenWidth = 700;
+const int screenHeight = 800;
+
 const int NOT_MATCH = 0;
 const int PARTIAL_MATCH = 1;
 const int MATCH = 2;
@@ -13,11 +16,12 @@ const int MATCH = 2;
 const int tileWidth = 65;
 const int tileHeight = 65;
 const int gap = 10;
+const int tileLeftPadding = 160;
 
 int row = 0;
 
 string word[5];
-int matches[5][5] = {0};
+int matches[5][5] = {0};  
 
 string getRandomWord()
 {
@@ -29,16 +33,16 @@ void genTiles() {
         for (int j = 0; j < 5; j++) {
             string character = string(1, word[j][i]);
             
-            DrawText(character.c_str(), 68 + i * (tileWidth + gap), 120 + j * (tileHeight + gap), 25, WHITE);
+            DrawText(character.c_str(), tileLeftPadding + 30 + i * (tileWidth + gap), 120 + j * (tileHeight + gap), 25, WHITE);
 
             if(matches[j][i] == MATCH){
-                DrawRectangleLines(50 + i * (tileWidth + gap), 100 + j * (tileHeight + gap), tileWidth, tileHeight, GREEN);
+                DrawRectangleLines(tileLeftPadding + i * (tileWidth + gap), 100 + j * (tileHeight + gap), tileWidth, tileHeight, GREEN);
             }
                 
             else if(matches[j][i] == PARTIAL_MATCH)
-                DrawRectangleLines(50 + i * (tileWidth + gap), 100 + j * (tileHeight + gap), tileWidth, tileHeight, YELLOW);
+                DrawRectangleLines(tileLeftPadding + i * (tileWidth + gap), 100 + j * (tileHeight + gap), tileWidth, tileHeight, YELLOW);
             else
-                DrawRectangleLines(50 + i * (tileWidth + gap), 100 + j * (tileHeight + gap), tileWidth, tileHeight, GRAY);
+                DrawRectangleLines(tileLeftPadding + i * (tileWidth + gap), 100 + j * (tileHeight + gap), tileWidth, tileHeight, GRAY);
         }
     }
 }
@@ -79,14 +83,11 @@ void checkWord(string &targerWord, string &word, int row){
 int main() {
     string targetWord = getRandomWord();
 
-    const int screenWidth = 1280;
-    const int screenHeight = 800;
-
     InitWindow(screenWidth, screenHeight, "Wordle Game");
     SetTargetFPS(60);
 
     // Define an array of alphabets
-    char alphabets[] = "QWERTYUIOP/nASDFGHJKLZXCVBNM";
+    char alphabets[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
     while (WindowShouldClose() == false) {
 
@@ -96,16 +97,22 @@ int main() {
         ClearBackground(BLACK);
 
         genTiles();
-
+        //Displaying the Keyboard and hadling the onClick for the keys
         for (int i = 0; i < 26; i++) {
-            float buttonX = 50 + i * 40;
+            
+            float buttonX = 50 + i * 60;
             float buttonY = 500;
 
-            if(alphabets[i] == '/n'){
-                cout << "N found";
+            if(i > 9){
+                buttonX = 80 + (i-10) * 60;
+                buttonY = buttonY + 60;
+                if(i > 18){
+                    buttonX = 140 + (i-19) * 60;
+                    buttonY = buttonY + 60;
+                }
             }
             // Draw button
-            if (GuiButton((Rectangle){ buttonX, buttonY, 40, 40 }, string(1, alphabets[i]).c_str())) {
+            if (GuiButton((Rectangle){ buttonX, buttonY, 60, 60 }, string(1, alphabets[i]).c_str())) {
                 word[row].push_back(alphabets[i]);
                 if((word[row].length() % 5) == 0){
                     checkWord(targetWord, word[row], row);
